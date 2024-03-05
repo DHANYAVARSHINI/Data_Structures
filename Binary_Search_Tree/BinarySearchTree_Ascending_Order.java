761637ddd7b4f5e9e0472a0fc3445114bbb0e159
DHANYAVARSHINI J <117835173+DHANYAVARSHINI@users.noreply.github.com>
2024-03-05 18:55:52 +0530

    Add files via upload

---
+++ Binary_Search_Tree/BinarySearchTree_Ascending_Order.java
@@ -0,0 +1,84 @@
+import java.util.*;
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int d){
+        data = d;
+        left = null; // Address of the left node
+        right = null;// Address of the left node
+    }
+}
+
+class BinarySearch{
+    TreeNode root = null;
+    
+    // Method to insert the node in to the tree
+    void insert(int d){
+        root = insert(root, d);
+    }
+    TreeNode insert(TreeNode curr, int d){
+        if(curr == null){
+            TreeNode node = new TreeNode(d);
+            return node; // Returns the address of the node
+        }
+        if(curr.data > d){ // If the data is less than the current node
+            curr.left = insert(curr.left, d);
+        }
+        else{// If the data is greater than the current node
+            curr.right = insert(curr.right, d);
+        }
+        return curr;
+    }
+    
+    // Method for get the output in preorder form
+    void pre_order(){
+        pre_order(root);
+    }
+    
+    void pre_order(TreeNode curr){
+        if(curr == null){
+            return;
+        }
+        System.out.print(curr.data+" ");
+        pre_order(curr.left);
+        pre_order(curr.right);
+    }
+    
+    // Method for get the output in inorder form
+    void in_order(){
+        in_order(root);
+    }
+    
+    void in_order(TreeNode curr){
+        if(curr == null){
+            return;
+        }
+        in_order(curr.left);
+        System.out.print(curr.data+" ");
+        in_order(curr.right);
+    }
+    
+}
+
+// Main Class
+class BinarySearchTree_Ascending_Order{
+    public static void main(String args[]){
+        BinarySearch bst = new BinarySearch();
+        
+        // Inserting the Datas in the tree
+        bst.insert(40);
+        bst.insert(20);
+        bst.insert(60);
+        bst.insert(10);
+        bst.insert(30);
+        
+        // Displaying the data in the pre-ordered form
+        bst.pre_order();
+        System.out.println("\n");
+        
+        // Displaying the data in the Ascending Order
+        System.out.println("Arranging the nodes in Ascending Order : ");
+        bst.in_order();
+    }
+}
