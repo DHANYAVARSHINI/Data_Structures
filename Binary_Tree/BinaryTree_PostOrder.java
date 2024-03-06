dcaeee7882dbe76959f634b674c28b0349a11284
DHANYAVARSHINI J <117835173+DHANYAVARSHINI@users.noreply.github.com>
2024-03-06 22:44:52 +0530

    Add files via upload

---
+++ Binary_Tree/BinaryTree_PostOrder.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+class treeNode{
+    int data;
+    treeNode left; //address of left child
+    treeNode right; //address of right child
+    public treeNode(int d){
+        data = d;
+        left = null;
+        right = null;
+    }
+}
+
+class Binary{
+    treeNode root = null;
+    void insert(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the root value: ");
+        int val = sc.nextInt();
+        if(val != -1){
+            treeNode node = new treeNode(val);
+            root = node;
+            insert(root);
+        }
+    }
+    void insert(treeNode curr){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the left value of "+curr.data+": ");
+        int left_val = sc.nextInt();
+        if(left_val !=-1){
+            treeNode node = new treeNode(left_val);
+            curr.left = node;
+            insert(curr.left);
+        }
+        System.out.println("Enter the right value of "+curr.data+": ");
+        int right_val = sc.nextInt();
+        if(right_val !=-1){
+            treeNode node = new treeNode(right_val);
+            curr.right = node;
+            insert(curr.right);
+        }
+    }
+    
+    // Method for Post-Order Tree Traversal
+    void post_order(){
+        in_order(root);
+    }
+    void post_order(treeNode curr){
+        if(curr == null){
+            return;
+        }
+        post_order(curr.left);
+        post_order(curr.right);
+        System.out.print(curr.data+" ");
+    }
+}
+
+// Main class
+class BinaryTree_PostOrder{
+    public static void main(String args[]){
+        Binary bt = new Binary();
+        
+        // Displaying the Inserting nodes
+        bt.insert();
+        
+        // Displaying the Post_Ordered Traversal
+        bt.post_order();
+        System.out.println();
+    }
+}
